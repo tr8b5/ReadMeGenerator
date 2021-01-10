caaeff6441be268572473a815a1afaa9b5622caf
William Miller <williamhmiller12@gmail.com>
2021-01-09 19:24:05 -0500

    I wrote the index.js logic for the readme Generator

--- index.js
+++ index.js
@@ -1,2 +1,80 @@
 // imports
 const inquirer = require("inquirer");
+const generatorMarkdown = require("./Markdown")
+const fs = require("fs"); 
+
+// Questions asked to the user
+const questions = [
+  {
+    type: "input",
+    message: "What is your GitHub username?",
+    name: "UserName",
+  },
+  {
+    type: "input",
+    message: "What is your email address?",
+    name: "Email",
+  },
+
+  {
+    type: "input",
+    message: "What is the title for your project?",
+    name: "Title",
+  },
+  {
+    type: "input",
+    message: "Please give description of your project.",
+    name: "Description",
+  },
+  {
+    type: "input",
+    message: "What necessary dependencies must be installed to run this app?",
+    name: "Installation",
+  },
+  {
+    type: "input",
+    message: "What is this app used for?",
+    name: "Usage",
+  },
+  {
+    type: "input",
+    message: "What license was used for this README?",
+    name: "License",
+  },
+
+  {
+    type: "input",
+    message: "Please add contributors",
+    name: "Contributor",
+  },
+  {
+    type: "input",
+    message: "What command do you use to test this App?",
+    name: "Test",
+  }
+];
+
+// Writing to a file 
+function writeToFile(fileName, data) {
+
+fs.writeFile("./demo/"+fileName, data, function(err) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log ("Successfully wrote: " + fileName);
+})
+
+}
+
+
+// initialization function
+function init() {
+  inquirer.prompt(questions)
+  .then(function(data) {
+    writeToFile("DemoREADME.md", generatorMarkdown(data));
+  })
+}
+
+
+// run the app
+init();
